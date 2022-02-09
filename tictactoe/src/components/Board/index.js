5d4e68990dc76805d0218848cd0de10b323c05ac
Macsen Sully-Morgan <macsullymorgan@gmail.com>
2022-02-09 14:46:16 +0000

    player move recorded, but not which box is clicked

--- tictactoe/src/components/Board/index.js
+++ tictactoe/src/components/Board/index.js
@@ -1,18 +1,37 @@
-import Square from './Square';
+import React from "react"
+import Square from '../Square';
+// import Move from "./Move"
 
 export default function Board(){
+    let player = true
+    var move = "x"
+    function playerMove({value}) {
+        if (player===true) {
+            move = "x"
+        } else if(player===false) {
+            move = "o"
+        }
+        player = player ? false:true;
+        console.log([move, value]) 
+    }
+
+
+//take in a player's click. DONE ALREADY
+//record the number and x/o of the click. DONE
+//toggle over from x/o to o/x for the next move. DONE 
+
 
     return (
         <div>
-            <Square value="1" />
-            <Square value="2" />
-            <Square value="3" />
-            <Square value="4" />
-            <Square value="5" />
-            <Square value="6" />
-            <Square value="7" />
-            <Square value="8" />
-            <Square value="9" />
+            <Square value="1" onClick={playerMove("")}/>
+            <Square value="2" onClick={() => playerMove("")}/>
+            <Square value="3" onClick={() => playerMove("")}/>
+            <Square value="4" onClick={() => playerMove("")}/>
+            <Square value="5" onClick={() => playerMove("")}/>
+            <Square value="6" onClick={() => playerMove("")}/>
+            <Square value="7" onClick={() => playerMove("")}/>
+            <Square value="8" onClick={() => playerMove("")}/>
+            <Square value="9" onClick={() => playerMove("")}/>
         </div>
     )
-}
+    }
--- tictactoe/src/components/Move/index.js
+++ tictactoe/src/components/Move/index.js
@@ -4,3 +4,17 @@
 
 //     )
 // };
+
+// import Square from "../Square";
+
+// export default function Move() {
+//     let player = true
+//     function playerMove() {
+
+//         player = player ? false:true; 
+//     }
+// }
+
+// //take in a player's click. DONE ALREADY
+// //record the number and x/o of the click.
+// //toggle over from x/o to o/x for the next move. DONE 
